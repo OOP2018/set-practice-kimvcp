f85a8165e333ae58319fbf1c6681ac62bc55bef3
Vichaphol Thamsuthikul <kim-ft@hotmail.com>
2018-02-08 21:29:03 +0700

    done

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Vichaphol Thamsuthikul 6010545897
+
 # Collections Practice
 
 These exercises demonstrate properties of some common collections.
@@ -16,13 +18,19 @@ See [Collections-practice](https://skeoop.github.io/week4/Collections-practice) 
 Answer the questions in the exercises here. Push your answer and code to Github Classroom.
 
 * When you print elements in the set, are they in same order as you added them?
-
+	= No, the data will be random.
+	
 * what is the command to remove all elements from a `set`?
-
+	- set.clear()
+	
 * When you add elements in a different order and print them, did the order change?
-
+	- Yes it does
+	
 * Can you add a duplicate "cat" and "dog" to set?
-
+	- No, the duplicate data can't be added.
+	
 * What is the result when you use TreeSet instead of HashSet?
-
+	- TreeSet will sort the data by alphabets but HashSet won't sort any data.
+	
 * Could we make `setExplorer` work with either Set or List?  How?
+	- Yes, we change the parameter to "Collection<String> @param name" ,so it will be able to use different set. Set and List are also the types of Collection.
--- src/SetPractice.java
+++ src/SetPractice.java
@@ -2,18 +2,17 @@ import java.util.*;
 
 /**
  * Practice using a Set.
+ * 
+ * @author Vichaphol Thamsuthikul
  */
 public class SetPractice {
 	/**
 	 * Add zero or more items to the set, and print what is being added.
-	 * 
-	 * 
-	 * 
-	 * 
+	 *
 	 * @parameter set is the Set to add items to
 	 * @parameter values zero or more Strings to add to set
 	 */
-	public static void addAll(Set<String> set, String... values) {
+	public static void addAll(Collection<String> set, String... values) {
 		for (String arg : values) {
 			System.out.print(" add " + arg);
 			set.add(arg);
@@ -23,8 +22,11 @@ public class SetPractice {
 
 	/**
 	 * Print all the elements in a set, using an Iterator.
+	 * 
+	 * @param set
+	 *            object
 	 */
-	public static void print(Set<String> set) {
+	public static void print(Collection<String> set) {
 		Iterator<String> iter = set.iterator();
 		while (iter.hasNext()) {
 			String string = iter.next();
@@ -34,10 +36,13 @@ public class SetPractice {
 
 	/**
 	 * Explore behavior of a Set.
+	 * 
+	 * @param set
+	 *            object
 	 */
-	public static void setExplorer( Set<String> set) {
+	public static void setExplorer(Collection<String> set) {
 		// 1. Create a HashSet and add strings: dog, ant, bird, elephant, cat
-		addAll(set, "dog", "cat", "ant");
+		addAll(set, "dog", "cat", "ant", "zebra", "ox");
 
 		// 2. Print elements in the set using an Iterator over the Set.
 		// You're going to need to do this again, so write a method print(Set set) to do
@@ -55,7 +60,7 @@ public class SetPractice {
 
 		// 5. Add same elements as exercise 1, but in a different order,
 		// such as: "cat", "elephant", "bird", "ant", "dog"
-		addAll(set, "cat", "dog", "ant");
+		addAll(set, "cat", "dog", "ant", "ox", "zebra");
 
 		// 6. Print the elements in the set. Is the order same as before?
 		print(set);
@@ -74,7 +79,7 @@ public class SetPractice {
 
 		// 9. Repeat the exercises using a TreeSet instead of HashSet.
 		// What is different when you use TreeSet?
-		
+
 		// Hint: Use Polymorphism. Make the Set object be a *parameter* to the
 		// method so you can easily use a different set just by changing the parameter!
 
@@ -82,7 +87,20 @@ public class SetPractice {
 	}
 
 	public static void main(String[] args) {
+		System.out.println("\n\tHashSet\n");
 		setExplorer(new HashSet<String>());
+		System.out.println("\n\tTreeSet\n");
 		setExplorer(new TreeSet<String>());
+		System.out.println("\n\tTreeSet(Comparator)\n");
+		Comparator<String> comp = new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				return o1.compareTo(o2);
+			}
+		};
+		setExplorer(new TreeSet<String>(comp));
+		System.out.println("\n\tList\n");
+		List<String> list = new ArrayList<String>();
+		setExplorer(list);
 	}
 }
